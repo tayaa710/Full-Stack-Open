6047d632c0243335e9f4fc4969713f407aac8092
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-01-20 20:04:43 +1300

    Continued on with part 1
    Reached part 1d) A function that returns a function

--- part1/Introduction to React/introdemo/src/App.jsx
+++ part1/Introduction to React/introdemo/src/App.jsx
@@ -1,9 +1,21 @@
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
+import { useState } from 'react'
 
+const App = () => {
+  const [value, setValue] = useState(10)
+  
+
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
   return (
     <div>
-      <p>{friends}</p>
+      {value}
+
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
---
+++ part1/JavaScript/playaround.js
@@ -0,0 +1,7 @@
+const t = [1, -1, 3]
+
+t.push(5)
+
+t.forEach(value => {
+  console.log(value)  // numbers 1, -1, 3, 5 are printed, each on its own line
+})                    
